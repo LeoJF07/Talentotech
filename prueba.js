39f22df07a8ac9a03b89bb0c54bda15aa57e1981
Leonardo <leo-akd@live.com.ar>
2024-11-14 21:39:10 -0300

    agregados estilos

---
+++ prueba.js
@@ -0,0 +1,12 @@
+console.log("probando desde archivo externo")
+console.log(10)
+console.log("10")
+
+
+console.log("la suma de 2 + 2 es: " + (2+2) ) /*unir*/
+
+/*datos booleanos*/
+
+console.log(true) //prendido 1
+console.log(false) //falso 0
+//usuario y contraseña : verdadero o falso
